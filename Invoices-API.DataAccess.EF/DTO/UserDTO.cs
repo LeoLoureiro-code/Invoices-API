ba8f41ecf1d2c8f246fc6e1e8af3ddb7da71f3e4
Leo <leolou.90@gmail.com>
2025-09-01 18:02:52 -0700

    Add DTO folder and User DTO

---
+++ Invoices-API.DataAccess.EF/DTO/UserDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invoices_API.DataAccess.EF.DTO
+{
+    public  class UserDTO
+    {
+
+        public string Email { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+
+        public string Role { get; set; } = null!;
+    }
+}
