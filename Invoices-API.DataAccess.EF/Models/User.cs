d458204304a58c58f91e58e6c74cd92e017eced4
Leo <leolou.90@gmail.com>
2025-09-01 18:22:47 -0700

    Add nulleable to Date times

--- Invoices-API.DataAccess.EF/Models/User.cs
+++ Invoices-API.DataAccess.EF/Models/User.cs
@@ -13,5 +13,15 @@ public partial class User
 
     public string Role { get; set; } = null!;
 
+    public string RefreshToken { get; set; } = null!;
+
+    public bool IsActive { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public DateTime? ExpiresAt { get; set; }
+
+    public DateTime? RevokedAt { get; set; }
+
     public virtual ICollection<Invoice> Invoices { get; set; } = new List<Invoice>();
 }
