6b4aaeb8585182b4b29e9b7d1d9280a626da8267
Leo <leolou.90@gmail.com>
2025-09-03 18:21:23 -0700

    Delete Create invoice DTO and put all in one DTO

--- Invoices-API.DataAccess.EF/DTO/InvoiceDTO.cs
+++ Invoices-API.DataAccess.EF/DTO/InvoiceDTO.cs
@@ -9,12 +9,36 @@ namespace Invoices_API.DataAccess.EF.DTO
 {
     public class InvoiceDto
     {
+
         public uint Id { get; set; }
+
+        public string? Street { get; set; }
+
+        public string? City { get; set; }
+
+        public string? PostCode { get; set; }
+
+        public string? Country { get; set; }
+
         public string? ClientName { get; set; }
+
         public string? ClientEmail { get; set; }
+
+        public string? ClientStreet { get; set; }
+
+        public string? ClientCity { get; set; }
+
+        public string? ClientPostCode { get; set; }
+
+        public string? ClientCountry { get; set; }
+
         public DateOnly? InvoiceDate { get; set; }
+
         public DateOnly? InvoicePayment { get; set; }
+
         public string? ProjectDescription { get; set; }
+        public int UserId { get; set; }
+
         public List<ItemListDTO> Items { get; set; } = new();
     }
 }
