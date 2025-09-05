3a12bee2f32fc72779d5ac98f6a81b12b5821386
Leo <leolou.90@gmail.com>
2025-09-04 18:41:35 -0700

    Add the methods to the interface to use them in the repository

---
+++ Invoices-API.DataAccess.EF/Repositories/Interfaces/IInvoiceRepository.cs
@@ -0,0 +1,23 @@
+﻿using Invoices_API.DataAccess.EF.DTO;
+using Invoices_API.DataAccess.EF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invoices_API.DataAccess.EF.Repositories.Interfaces
+{
+    public interface IInvoiceRepository
+    {
+        public Task<IEnumerable<Invoice>> GetAllInvoicesByUserId(int id);
+
+        public Task<Invoice> GetInvoiceById(int invoiceId, int userId);
+
+        public Task<Invoice> CreateInvoice(InvoiceDTO invoiceDTO, int userId);
+
+        public Task<Invoice> UpdateInvoice(InvoiceDTO invoiceDTO, int userId);
+
+        public Task DeleteInvoice(int id);
+    }
+}
