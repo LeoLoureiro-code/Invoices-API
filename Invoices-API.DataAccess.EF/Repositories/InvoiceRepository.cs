652f71f808b62120699cda50be4d010b37b8efd3
Leo <leolou.90@gmail.com>
2025-09-04 18:41:17 -0700

    Add methods to create, edit and delete invoices

--- Invoices-API.DataAccess.EF/Repositories/InvoiceRepository.cs
+++ Invoices-API.DataAccess.EF/Repositories/InvoiceRepository.cs
@@ -31,37 +31,76 @@ namespace Invoices_API.DataAccess.EF.Repositories
             return await _context.Invoices.FirstOrDefaultAsync(x => x.Id == invoiceId && x.UserId == userId);
         }
 
-        //public async Task<Invoice> CreateInvoice(CreateInvoiceDTO dto, int userId)
-        //{
-          
-        //    var invoice = new Invoice
-        //    {
-        //        Street = dto.Street,
-        //        City = dto.City,
-        //        PostCode = dto.PostCode,
-        //        Country = dto.Country,
-        //        ClientName = dto.ClientName,
-        //        ClientEmail = dto.ClientEmail,
-        //        ClientStreet = dto.ClientStreet,
-        //        ClientCity = dto.ClientCity,
-        //        ClientPostCode = dto.ClientPostCode,
-        //        ClientCountry = dto.ClientCountry,
-        //        InvoiceDate = dto.InvoiceDate,
-        //        InvoicePayment = dto.InvoicePayment,
-        //        ProjectDescription = dto.ProjectDescription,
-        //        UserId = userId,
-        //        ItemLists = dto.Items.Select(i => new ItemList
-        //        {
-        //            Name = i.Name,
-        //            Quantity = i.Quantity,
-        //            Price = i.Price
-        //        }).ToList()
-        //    };
+        //Add a way to put item list the moment the invoice is created
+        public async Task<Invoice> CreateInvoice(InvoiceDTO invoiceDTO, int userId)
+        {
 
-        //    await _context.Invoices.AddAsync(invoice);
-        //    await _context.SaveChangesAsync();
+            var invoice = new Invoice
+            {
+                Street = invoiceDTO.Street,
+                City = invoiceDTO.City,
+                PostCode = invoiceDTO.PostCode,
+                Country = invoiceDTO.Country,
+                ClientName = invoiceDTO.ClientName,
+                ClientEmail = invoiceDTO.ClientEmail,
+                ClientStreet = invoiceDTO.ClientStreet,
+                ClientCity = invoiceDTO.ClientCity,
+                ClientPostCode = invoiceDTO.ClientPostCode,
+                ClientCountry = invoiceDTO.ClientCountry,
+                InvoiceDate = invoiceDTO.InvoiceDate,
+                InvoicePayment = invoiceDTO.InvoicePayment,
+                ProjectDescription = invoiceDTO.ProjectDescription,
+                UserId = userId,
+            };
 
-        //    return invoice;
-        //}
+            await _context.Invoices.AddAsync(invoice);
+            await _context.SaveChangesAsync();
+
+            return invoice;
+        }
+
+        //Add a way to put item list the moment the invoice is edited
+        public async Task<Invoice> UpdateInvoice(InvoiceDTO invoiceDTO, int userId)
+        {
+            var existingInvoice = await _context.Invoices.FirstOrDefaultAsync(x => x.UserId == userId && x.UserId == userId);
+
+            if(existingInvoice == null)
+            {
+                throw new Exception("Invoice not found");
+            }
+
+            existingInvoice.Street = invoiceDTO.Street;
+            existingInvoice.City = invoiceDTO.City;
+            existingInvoice.PostCode = invoiceDTO.PostCode;
+            existingInvoice.Country = invoiceDTO.Country;
+            existingInvoice.ClientName = invoiceDTO.ClientName;
+            existingInvoice.ClientEmail = invoiceDTO.ClientEmail;
+            existingInvoice.ClientStreet = invoiceDTO.ClientStreet;
+            existingInvoice.ClientCity = invoiceDTO.ClientCity;
+            existingInvoice.ClientPostCode = invoiceDTO.ClientPostCode;
+            existingInvoice.ClientCountry = invoiceDTO.ClientCountry;
+            existingInvoice.InvoiceDate = invoiceDTO.InvoiceDate;
+            existingInvoice.InvoicePayment = invoiceDTO.InvoicePayment;
+            existingInvoice.ProjectDescription = invoiceDTO.ProjectDescription;
+
+            await _context.Invoices.AddAsync(existingInvoice);
+            await _context.SaveChangesAsync();
+            return existingInvoice;
+
+
+        }
+
+        public async Task DeleteInvoice(int id)
+        {
+            var existingInvoice = await _context.Invoices.FindAsync(id);
+
+            if (existingInvoice == null)
+            {
+                throw new Exception("Invoice not found");
+            }
+
+            _context.Invoices.Remove(existingInvoice);
+            await _context.SaveChangesAsync();
+        }
     }
 }
