e82d74b842e1686630a57b2f2237c38ecf687df5
Leo <leolou.90@gmail.com>
2025-09-07 18:59:45 -0700

    Create invoice controller and get invoices by user id endpoint

---
+++ Invoices-API/Controllers/InvoiceController.cs
@@ -0,0 +1,47 @@
+﻿using Invoices_API.DataAccess.EF.Context;
+using Invoices_API.DataAccess.EF.Models;
+using Invoices_API.DataAccess.EF.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Invoices_API.Controllers
+{
+    [Route("api/Invoice")]
+    [ApiController]
+    public class InvoiceController : ControllerBase
+    {
+
+        private readonly InvoiceRepository _invoiceRepository;
+
+        public InvoiceController(InvoiceRepository invoiceRepository)
+        {
+            _invoiceRepository = invoiceRepository;
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task <ActionResult <IEnumerable<Invoice>>> GetInvoicesByUserId(int id)
+        {
+            try
+            {
+                var invoices =  await _invoiceRepository.GetAllInvoicesByUserId(id);
+
+                if (invoices == null || !invoices.Any())
+                {
+                    return NotFound("No invoices found for this user");
+                }
+
+                return Ok(invoices);
+            }
+            catch(Exception ex)
+            {
+                return Problem(
+                    detail: ex.Message,
+                    title: "An error occurred while fetching invoices for this user.",
+                    statusCode: StatusCodes.Status500InternalServerError);
+            }
+            
+        }
+    }
+}
